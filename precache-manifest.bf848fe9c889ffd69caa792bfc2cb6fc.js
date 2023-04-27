073662eb86d5f188afdf0b1fa964f860b553978a
vijay-ram365 <ramkissoon.vijay@gmail.com>
2023-04-27 17:27:39 -0400

    Deploy React Weather App to GitHub

---
+++ precache-manifest.bf848fe9c889ffd69caa792bfc2cb6fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a890c9955ebf9528be5",
+    "url": "/react-weather-app/static/css/main.2249a9da.chunk.css"
+  },
+  {
+    "revision": "8a890c9955ebf9528be5",
+    "url": "/react-weather-app/static/js/main.d38528e2.chunk.js"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/react-weather-app/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "9e0b41a6a90a786e5ab15afd7cb304df",
+    "url": "/react-weather-app/index.html"
+  }
+];
